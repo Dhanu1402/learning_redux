dc1b6a856614d8965ec4c441dbc0a806eaf73888
Dhanu1402 <dhanukumar2000@gmail.com>
2023-03-30 21:15:01 +0530

    header done

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Learning Next.JS, Redux & Firebase
 
 1. Project setup
+
+2. Header section
+   1. npm install @heroicons/react@v1
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['links.papareact.com', 'fakestoreapi.com'], // domain from where to pull images
+  },
+};
